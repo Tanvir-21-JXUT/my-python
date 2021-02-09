ea998414b72cc546a7a078ed0fe2029917f4591f
Tanvir-21-Jxut <syedzilaniislamtanvir@gmail.com>
2021-02-10 02:21:34 +0800

    75%

--- tan.py
+++ tan.py
@@ -1,1 +1,29 @@
-# shkjo ljklasj
+class Fraction:
+    def __init__(self,n,m):
+
+        self.top = n
+        self.bottom = m
+
+    
+
+    def __str__(self) -> str:
+        return str(self.top)+"/"+ str(self.bottom)
+    
+    def __add__(self,other):
+        newtop = self.top * other.bottom + other.top * self.bottom
+        newbottom = self.bottom * other.bottom
+
+        print(Fraction(newtop,newbottom))
+        return(Fraction(newbottom,newtop))                                                                                                                                                                                                                                                                                                                                                                                                         
+    
+
+myfraction = Fraction(4,10)
+otherfraction = Fraction(3,8)
+f3 = myfraction + otherfraction
+f4 = otherfraction + myfraction
+k = myfraction.__str__()
+print(k)
+print(str(otherfraction))
+print(f4)
+print(f3)
+
